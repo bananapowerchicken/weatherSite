f9046fae15d9cee5e3cde2f91afbffa4fc86fde0
Анна МительковаÐ <221882@mail.ru>
2023-09-05 15:22:11 +0300

    first vers adapted bootstrap template

--- weatherSite/weather/urls.py
+++ weatherSite/weather/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),  # from file views connect template index (TODO)
+    path('', views.index),  # from file views connect template index
 ]
