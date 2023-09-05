68c399b489cbbdff1a28cef164f08b4471cde0a6
Анна МительковаÐ <221882@mail.ru>
2023-09-05 16:25:45 +0300

    updated requirements

--- weatherSite/weather/views.py
+++ weatherSite/weather/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
+    appid = '5f2887a8ea1643c8f436a30eeeec5f6e'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
+    
+    city = 'London'
+
     return render(request, 'weather/index.html')  # по умолчанию все шаблоны
 # ищутся в папке templates
 
