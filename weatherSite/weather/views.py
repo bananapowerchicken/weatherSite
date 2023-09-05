8343f6a501ad8fb915342bbc2ef37a3d57f84664
Анна МительковаÐ <221882@mail.ru>
2023-09-05 19:16:58 +0300

    weather to constant city is working

--- weatherSite/weather/views.py
+++ weatherSite/weather/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
     appid = '5f2887a8ea1643c8f436a30eeeec5f6e'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
-    
-    city = 'London'
+    # units=metric чтобы данные в цельсии
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
-    return render(request, 'weather/index.html')  # по умолчанию все шаблоны
+    city = 'Moscow'
+    res = requests.get(url.format(city)).json()
+
+    # словарь с нужными мне данными из запроса, можно расширять
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],  # извлекаем данные из json запроса
+        'icon:': res["weather"][0]["icon"]
+    }
+
+    # передаем эти данные в шаблон index.html
+    context = {
+        'info': city_info
+    }
+
+    return render(request, 'weather/index.html', context)  # по умолчанию все шаблоны
 # ищутся в папке templates
 
