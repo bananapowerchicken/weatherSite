b88dbf8aefa72c84813e6f4857279b3f44016bf6
Анна МительковаÐ <221882@mail.ru>
2023-12-01 17:40:11 +0300

    new html template, fixed icons

--- weatherSite/weather/views.py
+++ weatherSite/weather/views.py
@@ -9,34 +9,55 @@ def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
     # условие сохранения данных (нового города)
-    if (request.method == 'POST'):
-        form = CityForm(request.POST)
-        form.save()
+#     if (request.method == 'POST'):
+#         form = CityForm(request.POST)
+#         form.save()
     
-    # очистка формы
-    form = CityForm()  
+#     # очистка формы
+#     form = CityForm()  
 
-    cities = City.objects.all()
-    all_cities = []
+#     cities = City.objects.all()
+#     all_cities = []
 
-    for city in cities:
-        res = requests.get(url.format(city)).json()
+#     for city in cities:
+#         res = requests.get(url.format(city)).json()
+#         print(res)
 
-        # словарь с нужными мне данными из запроса, можно расширять
-        city_info = {
-            'city': city.name,
-            'temp': res["main"]["temp"],  # извлекаем данные из json запроса
-            'icon:': res["weather"][0]["icon"]
-        }
+#         # словарь с нужными мне данными из запроса, можно расширять
+#         city_info = {
+#             'city': city.name,
+#             'temp': res["main"]["temp"],  # извлекаем данные из json запроса
+#             'icon:': res["weather"][0]["icon"],
+#             'description:': res["weather"][0]["description"]
+#         }
+#         print(city_info)
 
-        all_cities.append(city_info)
+#         all_cities.append(city_info)
 
-    # передаем эти данные в шаблон index.html
-    context = {
-        'all_info': all_cities,
-        'form': form,
+#     # передаем эти данные в шаблон index.html
+#     context = {
+#         'all_info': all_cities,
+#         'form': form,
+#     }
+
+#     return render(request, 'weather/index.html', context)  # по умолчанию все шаблоны
+# # ищутся в папке templates
+
+    city = 'Munich'
+
+    res = requests.get(url.format(city)).json()
+    print(res)
+
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],  # извлекаем данные из json запроса
+        'icon': res["weather"][0]["icon"], # must come from the site
+        'description': res["weather"][0]["description"]
     }
+    print(city_info)
 
-    return render(request, 'weather/index.html', context)  # по умолчанию все шаблоны
-# ищутся в папке templates
+    # send info to context to send it to template
+    context = {'city_info': city_info}
+
+    return render(request, 'weather/index.html', context)
 
